ca20a5df54109a2658612560876c9a69c0a7e7a2
BackYard911 <yousseftarek9801@gmail.com>
2020-09-13 16:50:58 +0200

    added updatePosition

--- js/app.js
+++ js/app.js
@@ -17,12 +17,52 @@
  * Define Global Variables
  * 
 */
+let doc = document.querySelector("document");
+let body = document.querySelector("h1");
+let sec1 = document.querySelector("#section1");
+let sec2 = document.querySelector("#section2");
+let sec3 = document.querySelector("#section3");
+let sections = document.querySelectorAll("section");
+let mySection = [];
+for (const section of sections){
+    mySection.push(section.firstElementChild.firstElementChild);
+}
 
-let sec1 = document.querySelector("h1");
-sec1.style.color = "red";
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
 
+    );
+}
 
+console.log(mySection);
+function updatePosition(){
+    if(isInViewport(mySection[0])){
+        //add section1 to the header
+    }
+    else{
+        //remove section1 from the header
+    }
+    if(isInViewport(mySection[1])){
+        //add section2 to the header
+    }
+    else{
+        //remove section2 from the header
+    }
+    if(isInViewport(mySection[2])){
+        //add section3 to the header
+    }
+    else{
+        //remove section3 from the header
+    }
 
+}
+
+window.addEventListener("scroll",updatePosition);
 
 /**
  * End Global Variables
