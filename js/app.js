3905c4ce9bc6568b0d05c32df932966154462962
BackYard911 <yousseftarek9801@gmail.com>
2020-09-14 17:05:18 +0200

    added goToTop

--- js/app.js
+++ js/app.js
@@ -1,35 +1,19 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Define Global Variables
- * 
-*/
 let count = 0;
 let ul = document.querySelector("ul");
-let doc = document.querySelector("document");
-let body = document.querySelector("h1");
+let body = document.querySelector("html");
 let sec1 = document.querySelector("#section1");
 let sec2 = document.querySelector("#section2");
 let sec3 = document.querySelector("#section3");
 let sec4 = document.querySelector("#section4");
+let btnTop = document.querySelector("#goToTop");
 let sections = document.querySelectorAll("section");
 let mySection = [];
+
 for (const section of sections){
     mySection.push(section.firstElementChild.firstElementChild);
 }
+
+
 for (const section of mySection){
     
     let li = document.createElement('li');
@@ -43,11 +27,7 @@ for (const section of mySection){
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
     return (
-        rect.bottom-3 >= 0 //&&
-//        rect.left >= 0 &&
-//        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-//        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-
+        rect.bottom-3 >= 0
     );
 }
 
@@ -55,83 +35,42 @@ function goToSection(event){
     sections[event.target.counter].scrollIntoView({behavior : 'smooth'});
 }
 
-console.log(mySection);
+function goToTop(){
+    body.scrollIntoView({behavior : 'smooth'});
+}
+
 function updatePosition(){
     if(isInViewport(sec1)){
-        //add section1 to the header
         sec1.classList.add("your-active-class");
         sec2.classList.remove("your-active-class");
         sec3.classList.remove("your-active-class");
         sec4.classList.remove("your-active-class");
-        console.log("section1");
     }
-
+    
     else if (isInViewport(sec2)){
-        //add section2 to the header
         sec1.classList.remove("your-active-class");
         sec2.classList.add("your-active-class");
         sec3.classList.remove("your-active-class");
         sec4.classList.remove("your-active-class");
-        console.log("section2");
     }
     
     else if(isInViewport(sec3)){
-        //add section3 to the header
         sec1.classList.remove("your-active-class");
         sec2.classList.remove("your-active-class");
         sec3.classList.add("your-active-class");
         sec4.classList.remove("your-active-class");
-        console.log("section3");
     }
     
     else if(isInViewport(sec4)){
-        //add section3 to the header
         sec1.classList.remove("your-active-class");
         sec2.classList.remove("your-active-class");
         sec3.classList.remove("your-active-class");
         sec4.classList.add("your-active-class");
-        console.log("section4");
     }
-
+    
 }
 
 window.addEventListener("scroll",updatePosition);
-
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
+btnTop.addEventListener('click',goToTop);
 
 
